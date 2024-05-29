4cee9576ddb7edc0a314f94ec3ff4af70f59f62e
Anthony Fontivero <aafonti@outlook.com>
2024-05-29 17:32:17 +0100

    updated commit

--- main_windows.py
+++ main_windows.py
@@ -1,19 +1,23 @@
+import os
+print("Current Working Directory:", os.getcwd())
+
 import tkinter as tk
 from platform_specific.windows import platform_specific_setup
 
 def main():
-    root = tk.Tk()
-    root.geometry("500x300")
-    root.minsize(500, 300)  # Set minimum size
-    footer_text = "Created by ItchySudo. Windows v2.1.1"  # Define the footer text for Windows
-    app = platform_specific_setup(root, footer_text)
+    try:
+        root = tk.Tk()
+        root.geometry("500x300")
+        root.minsize(500, 300)  # Set minimum size
+        footer_text = "Created by ItchySudo. Windows v2.1.1"  # Define the footer text for Windows
+        print("Initializing platform-specific setup...")
+        app = platform_specific_setup(root, footer_text)
+        print("Platform-specific setup complete.")
 
-    # Center the footer text
-    footer_label = tk.Label(root, text=footer_text, font=("Consolas", 8), anchor="center")
-    footer_label.pack(side="bottom", fill="x")
-
-    root.mainloop()
-
+        print("Running main loop")
+        root.mainloop()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
